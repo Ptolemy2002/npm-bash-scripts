899946398e88621e7f81ee9381a367f77e87683e
Ptolemy2002 <ptolemy2002@gmail.com>
2025-12-23 15:46:48 -0500

    Refactor Windows path copying logic in build script

--- general/build.sh
+++ general/build.sh
@@ -17,6 +17,9 @@ if [ ! -z "$1" ]; then
     echo "Copying dist to Windows path: $1"
     # Make all necessary directories in the Windows path
     mkdir -p "/mnt/c/$1"
+    # Remove all existing files in the Windows path
+    rm -rf "/mnt/c/$1/*"
+    # Copy all files from dist to the Windows path
     cp -r dist/* "/mnt/c/$1"
     echo "Copy completed"
 fi
