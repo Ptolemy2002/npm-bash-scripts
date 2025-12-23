84ae32106321e627407a1c0df80a6e782f4acb27
Ptolemy2002 <ptolemy2002@gmail.com>
2025-12-23 15:26:45 -0500

    Added logic to copy public directory to dist if it exists in the build script

--- general/build.sh
+++ general/build.sh
@@ -3,4 +3,11 @@ echo "Removing previous build"
 rm -rf dist
 echo "Building the project"
 npm run _build
+
+# Copy public directory if it exists
+if [ -d "public" ]; then
+    echo "Copying public directory to dist"
+    cp -r public/* dist/
+fi
+
 echo "Finished building the project"
